ca887e8e45a6c32faa89efb0f001ce0e9653c04d
Yolol100 <Andrew_Baeten@hotmail.com>
2026-06-19 20:24:57 +0700

    Fix release metadata and README checks

--- assets/build/admin.asset.php
+++ assets/build/admin.asset.php
@@ -5,5 +5,5 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 return [
 	'dependencies' => [ 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n' ],
-	'version'      => '0.4.89',
+	'version'      => '0.4.91',
 ];
